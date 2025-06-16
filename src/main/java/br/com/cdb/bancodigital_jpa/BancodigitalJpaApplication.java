e1d3c6d8181bc3b44acf3c2b9f1ad851601d946b
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-16 10:52:22 -0300

    feat: Upload initial :sparkles:

---
+++ src/main/java/br/com/cdb/bancodigital_jpa/BancodigitalJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cdb.bancodigital_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancodigitalJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancodigitalJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cdb/bancodigital_jpa/BancodigitalJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdb.bancodigital_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
