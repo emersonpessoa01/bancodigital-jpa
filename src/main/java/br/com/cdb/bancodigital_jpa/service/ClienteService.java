588c401af7f1859111e7bf6b44f0a4c3fbda034a
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-16 11:28:20 -0300

    feat: Added getClientes :rocket:

--- src/main/java/br/com/cdb/bancodigital_jpa/service/ClienteService.java
+++ src/main/java/br/com/cdb/bancodigital_jpa/service/ClienteService.java
@@ -5,6 +5,8 @@ import br.com.cdb.bancodigital_jpa.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService
 {
@@ -12,10 +14,13 @@ public class ClienteService
     private ClienteRepository clienteRepository;
     public Cliente salvarCliente(String nome, Long cpf){
         //VALIDAR OS CAMPOS
-        Cliente cliente  = new Cliente;
+        // CRIAR O OBJETO CLIENTE
+        Cliente cliente  = new Cliente();
         cliente.setNome(nome);
         cliente.setCpf(cpf);
         return clienteRepository.save(cliente);
-        // CRIAR O OBJETO CLIENTE
+    }
+    public List<Cliente> getClientes() {
+        return clienteRepository.findAll();
     }
 }
